d4c1f9ed41222c907c743734621cff49e21e38f9
chiuko <chiuko@celloes.com>
2026-01-16 10:20:08 +0800

    fix

--- app/Http/Controllers/Api/CardCaptureController.php
+++ app/Http/Controllers/Api/CardCaptureController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Spatie\Browsershot\Browsershot;
 
 class CardCaptureController extends Controller
